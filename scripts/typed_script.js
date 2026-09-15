18649950bc8180fe6887dc2f4397861241511dfc
bit-EJ-Unioeste <contato.bitej@gmail.com>
2026-09-15 17:26:09 -0300

    Add files via upload

---
+++ scripts/typed_script.js
@@ -0,0 +1,11 @@
+new Typed("#typed", {
+  strings: [
+    "websites.",
+    "sistemas.",
+    "soluções tecnológicas."
+  ],
+  typeSpeed: 40,
+  backSpeed: 20,
+  backDelay: 1000,
+  loop: true
+});
